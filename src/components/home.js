419ed0ca12180c5cc3158e94fe3361c12a11662a
Jon1969Edwards <jon1969edwards@gmail.com>
2021-08-30 15:22:39 +0200

    Hello World

--- src/components/home.js
+++ src/components/home.js
@@ -1,50 +1,30 @@
-import React from "react";
-import Laptop from "../components/responsive/laptop";
-import Desktop from "../components/responsive/desktop";
-import Tablet from "../components/responsive/tablet";
-import Mobile from "../components/responsive/mobile";
-// import Doors from "../components/responsive/doors";
-import { useMediaQuery } from 'react-responsive';
+import '../App.css';
+import React from 'react';
+import Room from '../images/speakeasyRoom3.jpeg';
 
- 
-
-const Home = () => {
-  
-    const DesktopResponsive = ({ children }) => {
-        const isDesktop = useMediaQuery({ minWidth: 1550 })
-        return isDesktop ? children : null
-      }
-    const LaptopResponsive = ({ children }) => {
-        const isLaptop = useMediaQuery({ minWidth: 768, maxWidth: 1549 })
-        return isLaptop ? children : null
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                <img src={Room} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                </header>
+            </div>
+            
+        );
     }
-    const TabletResponsive = ({ children }) => {
-        const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 })
-        return isTablet ? children : null
-      }
-      const MobileResponsive = ({ children }) => {
-        const isMobile = useMediaQuery({ maxWidth: 767 })
-        return isMobile ? children : null
-      }
 
-
-    return (
-        <div className="container">
-          <DesktopResponsive>
-            <Desktop />
-          </DesktopResponsive>
-          <LaptopResponsive>
-            <Laptop />
-          </LaptopResponsive>
-          <TabletResponsive>
-            <Tablet />
-          </TabletResponsive>
-          <MobileResponsive>
-            <Mobile />
-          </MobileResponsive>
-        </div>
-    );
-  }
-
+}
 
 export default Home;
