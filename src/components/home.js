fbe32cbadc2a722e98b235a50abeddd81a5b0c98
Jon1969Edwards <jon1969edwards@gmail.com>
2021-08-31 12:31:46 +0200

    npm run deploy

--- src/components/home.js
+++ src/components/home.js
@@ -1,26 +1,77 @@
-import '../App.css';
-import '../style/styles.css';
-import React from 'react';
-import Room from '../images/speakeasyRoom3.jpeg';
-import Slides from './slides';
+// import '../App.css';
+// import '../style/styles.css';
+// import React from 'react';
+// import Room from '../images/speakeasyRoom3.jpeg';
+// import Slides from './slides';
 
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div>
-            <div className="slider">
-                <Slides />
-            </div>
-                <img src={Room} 
-                     className="background" 
-                     alt="background" 
-                />
-            </div>
+// class Home extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//             <div className="slider">
+//                 <Slides />
+//             </div>
+//                 <img src={Room} 
+//                      className="background" 
+//                      alt="background" 
+//                 />
+//             </div>
             
-        );
-    }
+//         );
+//     }
 
-}
+// }
+
+// export default Home;
+
+import React from "react";
+import Laptop from "../components/responsive/laptop";
+import Desktop from "../components/responsive/desktop";
+import Tablet from "../components/responsive/tablet";
+import Mobile from "../components/responsive/mobile";
+// import Doors from "../components/responsive/doors";
+import { useMediaQuery } from 'react-responsive';
+
+ 
+
+const Home = () => {
+  
+    const DesktopResponsive = ({ children }) => {
+        const isDesktop = useMediaQuery({ minWidth: 1550 })
+        return isDesktop ? children : null
+      }
+    const LaptopResponsive = ({ children }) => {
+        const isLaptop = useMediaQuery({ minWidth: 768, maxWidth: 1549 })
+        return isLaptop ? children : null
+    }
+    const TabletResponsive = ({ children }) => {
+        const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 })
+        return isTablet ? children : null
+      }
+      const MobileResponsive = ({ children }) => {
+        const isMobile = useMediaQuery({ maxWidth: 767 })
+        return isMobile ? children : null
+      }
+
+
+    return (
+        <div className="container">
+          <DesktopResponsive>
+            <Desktop />
+          </DesktopResponsive>
+          <LaptopResponsive>
+            <Laptop />
+          </LaptopResponsive>
+          <TabletResponsive>
+            <Tablet />
+          </TabletResponsive>
+          <MobileResponsive>
+            <Mobile />
+          </MobileResponsive>
+        </div>
+    );
+  }
+
 
 export default Home;
