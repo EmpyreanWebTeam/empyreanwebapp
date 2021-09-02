3bdcfa794b1d54bd18ddfa11cc52d468fc5c7c35
Jon1969Edwards <jon1969edwards@gmail.com>
2021-09-02 13:52:22 +0200

    Switches arrow buttons around

--- src/components/slides.js
+++ src/components/slides.js
@@ -211,13 +211,13 @@ function Slide({ slide, offset }) {
     return (
         
           <div className="slides">
-            <button onClick={() => dispatch({ type: "PREV" })}>‹</button>
+            <button onClick={() => dispatch({ type: "PREV" })}>›</button>
   
               {[...slides, ...slides, ...slides].map((slide, i) => {
                 let offset = slides.length + (state.slideIndex - i);
                 return <Slide slide={slide} offset={offset} key={slide.id} />;
               })}
-            <button onClick={() => dispatch({ type: "NEXT" })}>›</button>
+            <button onClick={() => dispatch({ type: "NEXT" })}>‹</button>
           </div>
     )
   }
